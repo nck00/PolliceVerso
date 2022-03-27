2e65f4c2e772a466c0a28bf67d790664b3c13874
nck00 <50334352+nck00@users.noreply.github.com>
2022-03-27 20:23:27 +0200

    Barebones image display and Judgement button
    Images scales based on maximum height

--- pv_main.py
+++ pv_main.py
@@ -1,4 +1,5 @@
 from PyQt6.QtCore import *
+from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6 import uic
 import os
@@ -9,10 +10,16 @@ class PolliceVerso(QMainWindow):
         super().__init__()
         uic.loadUi("pv.ui", self)
         self.initUi()
+        # to be filled by the user
+        self.posImages = []
+        self.negImages = []
+        self.listOfPics = []
 
     def initUi(self) -> None:
         self.actionSetup.triggered.connect(self.on_ActionSetup_triggered)
+        self.actionJudge.triggered.connect(self.on_ActionJudge_triggered)
         self.actionAbout.triggered.connect(self.on_ActionAbout_triggered)
+        self.statusbar.showMessage("File -> Setup to begin.")
         self.show()
 
     @pyqtSlot()
@@ -21,20 +28,29 @@ class PolliceVerso(QMainWindow):
         - Where are the pics? (folderPath: str)
         - Which filetypes? (suffixes: list)
         - Include subfolders? (recursive: bool)
+        - What to do with positively judged pics? (pos2do: str)
+        - What to do with negatively judged pics? (neg2do: str)
         If all questions are answered call getListOfPics
-        TODO: Add suffix and recursive Dialogs
+        TODO: Add suffix, recursive and judgement Dialogs
         """
         folderPath = QFileDialog.getExistingDirectory(self, "Select Folder")
         suffixes = (".jpg", ".gif", ".png")
+        neg2do = "txt"
+        pos2do = "txt"
         recursive = False
         if folderPath and recursive is not None:
-            self.getListOfPics(folderPath, recursive, suffixes)           
+            # TODO: Add dialog if listOfPics exists to ask if to extend or to replace
+            self.listOfPics = self.getListOfPics(folderPath, recursive, suffixes)           
 
     @pyqtSlot()
-    def on_ActionAbout_triggered(self):
-        about = QDialog(self)
-        about.setWindowTitle("About Pollice Verso")
-        about.exec()
+    def on_ActionJudge_triggered(self):
+        if not self.listOfPics:
+            self.statusbar.showMessage("File -> Setup to begin!")
+            return
+        i = 0
+        currentPic = self.listOfPics[i]
+        self.picLabel.setPixmap(QPixmap(currentPic).scaledToHeight(self.picLabel.size().height()))
+        
 
     def getListOfPics(self, folderPath: str, recursive: bool, suffixes: list) -> list:
         """
@@ -46,7 +62,17 @@ class PolliceVerso(QMainWindow):
             for entry in path:
                 if entry.is_file() and os.path.splitext(entry)[1] in suffixes:
                     listOfPics.append(os.path.normpath(entry.path))
+        if listOfPics: # atleast one image
+            self.statusbar.showMessage(f"{len(listOfPics)} Pictures found in {folderPath}. File -> Judge to proceed.")
+        else:
+            self.statusbar.showMessage(f"{len(listOfPics)} Pictures found in {folderPath}.")
         return listOfPics
+
+    @pyqtSlot()
+    def on_ActionAbout_triggered(self):
+        about = QDialog(self)
+        about.setWindowTitle("About Pollice Verso")
+        about.exec()
 
     @pyqtSlot()
     def on_posPushButton_clicked(self):
