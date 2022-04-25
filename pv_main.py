6b69ce05e52c8abbbebcbc36fadebf69e47df3d4
nck00 <50334352+nck00@users.noreply.github.com>
2022-04-25 21:01:08 +0200

    Added dialog stub for what to do with judged pictures

--- pv_main.py
+++ pv_main.py
@@ -37,12 +37,25 @@ class PolliceVerso(QMainWindow):
         """
         folderPath = QFileDialog.getExistingDirectory(self, "Select Folder")
         suffixes = (".jpg", ".gif", ".png", ".webp")
-        self.neg2do = "txt"
-        self.pos2do = "txt"
+        self.neg2do = self.askToDo("negatively")
+        self.pos2do = self.askToDo("positively")
         recursive = self.askRecursive()
         if folderPath and recursive is not None:
             # TODO: Add dialog if listOfPics exists to ask if to extend or to replace
             self.listOfPics = self.getListOfPics(folderPath, recursive, suffixes)           
+
+    def askToDo(self, choice: str) -> str:
+        toDoQuestion = QDialog()
+        toDoQuestion.setWindowTitle(f"{choice[:1].upper()}{choice[1:]}?")
+        toDoLayout = QVBoxLayout()
+        toDoComboBox = QComboBox(toDoQuestion)
+        toDoLabel = QLabel(toDoQuestion)
+        toDoLabel.setText(f"What should happen with {choice} evaluated images?")
+        toDoComboBox.addItems(("txt", "copy", "delete"))
+        toDoLayout.addWidget(toDoLabel)
+        toDoLayout.addWidget(toDoComboBox)
+        toDoQuestion.setLayout(toDoLayout)
+        toDo = toDoQuestion.exec()
 
     def askRecursive(self):
         recursiveQuestion = QMessageBox()
